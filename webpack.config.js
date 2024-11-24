d1489a1f70775109756f35f12872ad6f37eab953
sakaschekolda <ashelkunov1904@gmail.com>
2024-11-24 23:57:52 +0400

    final: LAB4-3 Добавлен финальный проект со всеми конфигами

--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,63 @@
-const path = require('path') // Импортируем модуль "path" для работы с путями файлов
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-module.exports = {
-	mode: 'development',
-	entry: './src/index.js', // Точка входа для сборки проекта
-
-	output: {
-		filename: 'bundle.js', // Имя выходного файла сборки
-		path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
-		assetModuleFilename: 'images/picture.svg',
-	},
-
-	devServer: {
-		liveReload: true,
-	},
-
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			  },
-			  {
-				test: /\.(png|jpe?g|gif|svg)$/,
-				type: 'asset/resource',
-			  },
-			{
-				test: /\.svg$/,
-				use: [
-				  {
-					loader: 'svg-url-loader',
-					options: {
-					  limit: 8192, // Определяет порог, выше которого будет использоваться отдельный файл
-					},
-				  },
-				],
-			  },
-		],
-	},
-
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'index.html',
-		}),
-        new HtmlWebpackPlugin({
-			template: './src/add.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'add.html',
-		}),
-		new HtmlWebpackPlugin({
-			template: './src/main.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'main.html',
-		}),
-		new HtmlWebpackPlugin({
-			template: './src/projects.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'projects.html',
-		}),
-		new HtmlWebpackPlugin({
-			template: './src/tasks.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'tasks.html',
-		}),
-		new HtmlWebpackPlugin({
-			template: './src/about.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'about.html',
-		}),
-	],
-
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist'), // Каталог для статики
-		},
-		open: true, // Автоматически открывать браузер
-	},
-
-	mode: 'development', // Режим сборки
-}
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Добавляем плагин 
+ 
+module.exports = { 
+    entry: './src/index.js', 
+    output: { 
+        filename: 'bundle.js', 
+        path: path.resolve(__dirname, 'dist'), 
+    }, 
+    module: { 
+        rules: [ 
+            { 
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader'], 
+            }, 
+        ], 
+    }, 
+    plugins: [ 
+        new HtmlWebpackPlugin({ 
+            template: './src/index.html', 
+            inject: true, 
+            chunks: ['index'], 
+            filename: 'index.html', 
+        }), 
+        new HtmlWebpackPlugin({ 
+            template: './src/about.html', 
+            inject: true, 
+            chunks: ['about'], 
+            filename: 'about.html', 
+        }), 
+        new HtmlWebpackPlugin({ 
+            template: './src/projects.html', 
+            inject: true, 
+            chunks: ['projects'], 
+            filename: 'projects.html', 
+        }), 
+        new HtmlWebpackPlugin({ 
+            template: './src/tasks.html', 
+            inject: true, 
+            chunks: ['tasks'], 
+            filename: 'tasks.html', 
+        }), 
+        new HtmlWebpackPlugin({ 
+            template: './src/add.html', 
+            inject: true, 
+            chunks: ['add'], 
+            filename: 'add.html', 
+        }), 
+        new CopyWebpackPlugin({ 
+            patterns: [ 
+                { from: './src/images/favicon-32x32.ico', to: 'favicon.ico' }, // Копируем favicon.ico 
+            ], 
+        }), 
+    ], 
+    devServer: { 
+        static: { 
+            directory: path.join(__dirname, 'dist'), 
+        }, 
+        open: true, 
+    }, 
+    mode: 'development', 
+};
